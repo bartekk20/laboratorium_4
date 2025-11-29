004757da401f48f778df6c36545cacd11e0c18e3
Bartłomiej Kwiatkowski <73147@student.wsiz.edu.pl>
2025-11-29 10:11:03 +0100

    zad1

---
+++ zadanie1.py
@@ -0,0 +1,40 @@
+Moja_lista = [1, 17, 3, 5, 3, 4, 86, 90, 2, 21]
+lista_dodatkowa = [100, 200, 300]
+print(Moja_lista[0])
+
+print(Moja_lista[-1])
+
+print(len(Moja_lista))
+
+print(max(Moja_lista))
+
+print(min(Moja_lista))
+
+print(sum(Moja_lista))
+
+print(sorted(Moja_lista))
+
+Moja_lista.append(6)
+print(Moja_lista)
+
+Moja_lista.insert(99, 5)
+print(Moja_lista)
+
+print(Moja_lista.pop())
+
+print(Moja_lista.reverse())
+
+lista_inna = Moja_lista + lista_dodatkowa
+
+print(lista_inna)
+
+print(Moja_lista * 5)
+
+print(Moja_lista[:5])
+
+print(Moja_lista[5:])
+
+print(Moja_lista[5:2:1])
+
+print(Moja_lista[::-1])
+
